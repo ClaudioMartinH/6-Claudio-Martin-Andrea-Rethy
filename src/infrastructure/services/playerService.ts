622cb153353491976af65c2df45462bd361d2022
ClaudioMartinH <160405164+ClaudioMartinH@users.noreply.github.com>
2024-09-01 00:23:25 +0200

    player service done

---
+++ src/infrastructure/services/playerService.ts
@@ -0,0 +1,38 @@
+import { PlayerRepositoryImpl } from "../repositories/playerRepositoryImpl.js";
+import { Player } from "@prisma/client";
+
+export class PlayerService {
+  private playerRepository: PlayerRepositoryImpl;
+  constructor() {
+    this.playerRepository = new PlayerRepositoryImpl();
+  }
+
+  async getAllPlayers(): Promise<Player[]> {
+    return await this.playerRepository.getAllPlayers();
+  }
+
+  async getPlayerById(id: number): Promise<Player | null> {
+    const player = await this.playerRepository.getPlayerById(id);
+    if (!player) {
+      throw new Error("Player not found");
+    }
+    return player;
+  }
+
+  async createPlayer(
+    data: Omit<Player, "id" | "register_date">
+  ): Promise<Player> {
+    return await this.playerRepository.createPlayer(data);
+  }
+
+  async updatePlayer(
+    id: number,
+    data: Partial<Omit<Player, "id">>
+  ): Promise<Player | null> {
+    const player = await this.playerRepository.getPlayerById(id);
+    if (!player) {
+      throw new Error("Player not found");
+    }
+    return await this.playerRepository.updatePlayer(id, data);
+  }
+}
