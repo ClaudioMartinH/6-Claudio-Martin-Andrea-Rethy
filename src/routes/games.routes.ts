4795f85189c01dcebb0f8201f84b3d0f6d184288
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-09-02 17:53:08 +0200

    Update games.routes

--- src/routes/games.routes.ts
+++ src/routes/games.routes.ts
@@ -1,20 +1,26 @@
 import { Router } from "express";
-import { prisma } from "../db.js";
+import { GameController } from "src/infrastructure/controllers/gameController.js";
 
 const router = Router();
+const gameController = new GameController();
 
-router.get("/games", async (_req, res) => {
-  const games = await prisma.game.findMany();
-  res.json(games);
-});
+router.get("/playerGames/:id", gameController.getGamesForPlayer);
+router.post("/playerGames/:id", gameController.createGame);
+router.delete("/playerGames/:id", gameController.deleteGames);
 
-router.delete("/games/:id", async (req, res) => {
-  const deletedGame = await prisma.game.delete({
-    where: { id: parseInt(req.params.id) },
-  });
-  if (!deletedGame)
-    return res.status(404).json({ error: "Game not found" });
-  return res.json(deletedGame);
-});
+
+// router.get("/games", async (_req, res) => {
+//   const games = await prisma.game.findMany();
+//   res.json(games);
+// });
+
+// router.delete("/games/:id", async (req, res) => {
+//   const deletedGame = await prisma.game.delete({
+//     where: { id: parseInt(req.params.id) },
+//   });
+//   if (!deletedGame)
+//     return res.status(404).json({ error: "Game not found" });
+//   return res.json(deletedGame);
+// });
 
 export default router;
