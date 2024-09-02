7fc9a73f9b0b26a9df6d70c03af3761880406f75
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-09-02 16:47:35 +0200

    Update GameRepository, finish gameRepositoryImpl

--- src/domain/repositories/GameRepository.ts
+++ src/domain/repositories/GameRepository.ts
@@ -1,6 +1,6 @@
 import { PlayerGame } from "../entities/PlayerGame.js";
 export interface GameRepository {
     getGamesForPlayer(playerId: Number): Promise<PlayerGame[]>;
-    createGame(playerId: Number): Promise<PlayerGame>;
-    deleteGame(playerId: Number): Promise<void>;
+    createGame(game: PlayerGame): Promise<PlayerGame>;
+    deleteGames(playerId: Number): Promise<number>;
 }
--- src/infrastructure/repositories/gameRepositoryImpl.ts
+++ src/infrastructure/repositories/gameRepositoryImpl.ts
@@ -3,9 +3,28 @@ import { PrismaClient, PlayerGame } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export class GameRepositoryImpl {
-    async getGamesForPlayer(playerId: Number): Promise<PlayerGame[]> {
+    async getGamesForPlayer(playerId: number): Promise<PlayerGame[]> {
         return await prisma.playerGame.findMany({
             where: { playerId },
         });
     }
+
+    async createGame(data: Omit<PlayerGame, "id">): Promise<PlayerGame> {
+        return await prisma.playerGame.create({
+            data: {
+                ...data,
+              },
+        });
+    }
+    
+    async deleteGames(playerId: number): Promise<number> {
+        try {
+            const result = await prisma.playerGame.deleteMany({
+                where: { playerId },
+            });
+            return result.count;
+        } catch (error) {
+            throw new Error(`Player not found or error deleting games: ${error}`);
+        }
+    }    
 }
