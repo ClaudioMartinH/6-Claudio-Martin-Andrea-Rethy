75bfd7c5e9e7daa42025302361e233202cf09a12
ClaudioMartinH <160405164+ClaudioMartinH@users.noreply.github.com>
2024-09-04 21:27:57 +0200

    changed paths to pass CI

--- src/infrastructure/services/rankingService.ts
+++ src/infrastructure/services/rankingService.ts
@@ -1,21 +1,21 @@
-import { RankingRepositoryImpl } from "../repositories/rankingRepositoryImpl";
+import { RankingRepositoryImpl } from "../repositories/rankingRepositoryImpl.js";
 import { Ranking } from "@prisma/client";
 
 export class RankingService {
-    private rankingRepository: RankingRepositoryImpl;
-    constructor(){
-        this.rankingRepository = new RankingRepositoryImpl();
-    }
+  private rankingRepository: RankingRepositoryImpl;
+  constructor() {
+    this.rankingRepository = new RankingRepositoryImpl();
+  }
 
-    async getRanking(): Promise<Ranking[]> {
-        return await this.rankingRepository.getRanking();
-    };
+  async getRanking(): Promise<Ranking[]> {
+    return await this.rankingRepository.getRanking();
+  }
 
-    async getLoser(): Promise<Ranking | null> {
-        return await this.rankingRepository.getLoser();
-    };
+  async getLoser(): Promise<Ranking | null> {
+    return await this.rankingRepository.getLoser();
+  }
 
-    async getWinner(): Promise<Ranking | null> {
-        return await this.rankingRepository.getWinner();
-    };
+  async getWinner(): Promise<Ranking | null> {
+    return await this.rankingRepository.getWinner();
+  }
 }
