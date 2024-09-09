bdf64c39f018f51feda8e39b4fd20b5ac284fbb4
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-09-09 18:38:11 +0200

    Borrar carpetas y archivos antiguas

--- src/domain/repositories/GameRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Game } from "../entities/Game.js";
-export interface GameRepository {
-    getGamesForPlayer(playerId: Number): Promise<Game[]>;
-    createGame(game: Game): Promise<Game>;
-    deleteGames(playerId: Number): Promise<number>;
-}
--- src/domain/repositories/RankingRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Ranking } from "../entities/Ranking.js";
-export interface RankingRepository {
-  getRanking(): Promise<Ranking[]>;
-  getLoser(): Promise<Ranking>;
-  getWinner(): Promise<Ranking>;
-}
