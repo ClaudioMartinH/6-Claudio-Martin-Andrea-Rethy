452ff19aa8c03af375f524a6f14944bc7b9aa18e
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-09-19 17:14:05 +0200

    Update docker settings and proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 5173,
-    open: true,
+    // port: 5173,
     proxy: {
-      "/api": {
-        target: "http://backend:3000",
+      '/api': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
 });
+
