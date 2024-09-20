e71c1ec8360305e444d4642597816b57a82f2313
ClaudioMartinH <160405164+ClaudioMartinH@users.noreply.github.com>
2024-09-21 00:43:47 +0200

    fixed docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
       "/api": {
         target: "http://backend:3000",
         changeOrigin: true,
+        secure: false,
       },
     },
   },
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react()],
+  plugins: [react()],
   server: {
-    // port: 5173,
+    port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://backend:3000",
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
-
