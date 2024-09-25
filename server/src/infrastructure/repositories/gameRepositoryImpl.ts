99cdb031cb2f3f522e03541800b4061a23f3a63e
Claudio Martin <klaudito46@gmail.com>
2024-09-25 13:56:38 +0200

    fixed db ranking issues

--- server/src/infrastructure/repositories/gameRepositoryImpl.ts
+++ server/src/infrastructure/repositories/gameRepositoryImpl.ts
@@ -10,11 +10,61 @@ export class GameRepositoryImpl {
     }
 
     async createGame(data: Omit<Game, "id">): Promise<Game> {
-        return await prisma.game.create({
-            data: {
-                ...data,
-              },
-        });
+        const newGame = await prisma.game.create({
+                data: {
+                    ...data,
+                },
+            })
+            if (data.overallResult === "Win") {
+                const updatedRanking = await prisma.ranking.upsert({
+                    where: { playerId: data.playerId },
+                    update: {
+                        totalGames: { increment: 1},
+                        totalWins: { increment: 1}
+                    },
+                    create: {
+                        playerId: data.playerId,
+                        totalGames: 1,
+                        totalWins: 1,
+                    },
+                });
+                const percentage = (updatedRanking.totalWins / updatedRanking.totalGames) * 100;
+                await prisma.ranking.upsert({
+                    where: { playerId: data.playerId },
+                    update: {
+                        winPercentage: percentage,
+                    },
+                    create: {
+                        playerId: data.playerId,
+                        winPercentage: percentage,
+                    },
+                })
+            } else {
+                const updatedRanking = await prisma.ranking.upsert({
+                    where: { playerId: data.playerId },
+                    update: {
+                        totalGames: { increment: 1},
+                        totalLost: { increment: 1}
+                    },
+                    create: {
+                        playerId: data.playerId,
+                        totalGames: 1,
+                        totalLost: 1,
+                    },
+                });
+                const percentage = (updatedRanking.totalLost / updatedRanking.totalGames) * 100;
+                await prisma.ranking.upsert({
+                    where: { playerId: data.playerId },
+                    update: {
+                        winPercentage: percentage,
+                    },
+                    create: {
+                        playerId: data.playerId,
+                        winPercentage: percentage,
+                    },
+                })
+            }
+        return newGame;
     }
     
     async deleteGames(playerId: number): Promise<number> {
